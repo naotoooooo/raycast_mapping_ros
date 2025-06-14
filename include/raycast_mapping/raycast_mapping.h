19ddf62fdcba947b16c94da25e05dd6b1cffe60e
naotoooooo <ee213099@meiji.ac.jp>
2025-06-14 10:57:57 +0000

    feat：timer_callbackを使い，画像を読み取ってコストマップまで作成(プログラム汚い)

--- include/raycast_mapping/raycast_mapping.h
+++ include/raycast_mapping/raycast_mapping.h
@@ -55,11 +55,10 @@ public:
 
 private:
   /**
-   * @brief Callback function for point cloud
+   * @brief Timer callback function
    *
-   * @param msg Point cloud message
    */
-  void cloud_callback(const sensor_msgs::PointCloud2::ConstPtr &msg);
+  void timer_callback(const ros::TimerEvent &);
 
   /**
    * @brief Create precast database
@@ -90,32 +89,12 @@ private:
   int calc_angle_id(float angle, const float yaw_reso);
 
   /**
-   * @brief Create map
-   *
-   * @param cloud Point cloud
-   * @param precast_db Precast database
-   * @return nav_msgs::OccupancyGrid Local map
-   */
-  nav_msgs::OccupancyGrid create_map(const sensor_msgs::PointCloud2 &cloud, const PrecastDB &precast_db);
-
-  /**
    * @brief Initialize map
    *
    * @param map_info Map information
    * @return nav_msgs::OccupancyGrid Initialized map
    */
   nav_msgs::OccupancyGrid init_map(const nav_msgs::MapMetaData &map_info);
-
-  /**
-   * @brief Check if the point is in the map
-   *
-   * @param dist Distance
-   * @param angle Angle
-   * @param map_info Map information
-   * @return true If the point is in the map
-   * @return false If the point is not in the map
-   */
-  bool in_map(const float dist, const float angle, const nav_msgs::MapMetaData &map_info);
 
   /**
    * @brief Convert x, y to grid index
@@ -127,6 +106,15 @@ private:
    */
   int xy_to_grid_index(const float x, const float y, const nav_msgs::MapMetaData &map_info);
 
+
+  /**
+   * @brief Make images for visualization
+   *
+   * @param free_color cost-free color
+   * @param image_path Path to images
+   */
+  nav_msgs::OccupancyGrid make_images(cv::Vec3b free_color,std::string image_path, const PrecastDB &precast_db);  // 必要に応じてパスを変更
+
   std::string frame_id_;
   float map_reso_;  // [m/cell]
   float map_size_;
@@ -137,6 +125,7 @@ private:
   ros::NodeHandle private_nh_;
   ros::Publisher map_pub_;
   ros::Subscriber cloud_sub_;
+  ros::Timer timer_;
 
   tf2_ros::Buffer tf_buffer_;
   tf2_ros::TransformListener tf_listener_;
--- src/raycast_mapping.cpp
+++ src/raycast_mapping.cpp
@@ -8,6 +8,8 @@
 #include <string>
 #include <utility>
 #include <vector>
+#include <opencv2/opencv.hpp>
+
 
 #include "raycast_mapping/raycast_mapping.h"
 
@@ -19,7 +21,9 @@ LocalMapCreator::LocalMapCreator(void) : private_nh_("~"), tf_listener_(tf_buffe
   private_nh_.param<float>("yaw_reso", yaw_reso_, 0.087);
 
   map_pub_ = nh_.advertise<nav_msgs::OccupancyGrid>("/local_map", 1);
-  cloud_sub_ = nh_.subscribe("/cloud", 1, &LocalMapCreator::cloud_callback, this);
+  // cloud_sub_ = nh_.subscribe("/cloud", 1, &LocalMapCreator::cloud_callback, this);
+  // タイマーの設定（1秒ごとにコールバックを呼び出す）
+  timer_ = nh_.createTimer(ros::Duration(1.0), &LocalMapCreator::timer_callback, this);
 
   ROS_INFO_STREAM(ros::this_node::getName() << " node has started..");
   ROS_INFO_STREAM("frame_id: " << frame_id_);
@@ -28,35 +32,60 @@ LocalMapCreator::LocalMapCreator(void) : private_nh_("~"), tf_listener_(tf_buffe
   ROS_INFO_STREAM("yaw_reso: " << yaw_reso_);
 
   precast_db_ = create_precast_db(map_reso_, map_size_, yaw_reso_);
+
+
+  //追記
+
+  //カラーマップの定義（Cityscapesのようなデータセットの場合）
+  // Cscolor = np.array([
+  //     [128, 64, 128],  // Road
+  //     [244, 35, 232],  // Sidewalk
+  //     [70, 70, 70],    // Building
+  //     [102, 102, 156], // Wall
+  //     [190, 153, 153], // Fence
+  //     [153, 153, 153], // Pole
+  //     [250, 170, 30],  // Traffic light
+  //     [220, 220, 0],   // Traffic sign
+  //     [107, 142, 35],  // Vegetation
+  //     [152, 251, 152], // Terrain
+  //     [70, 130, 180],  // Sky
+  //     [220, 20, 60],   // Person
+  //     [255, 0, 0],     // Rider
+  //     [0, 0, 142],     // Car
+  //     [0, 0, 70],      // Truck
+  //     [0, 60, 100],    // Bus
+  //     [0, 80, 100],    // Train
+  //     [0, 0, 230],     // Motorcycle
+  //     [119, 11, 32],   // Bicycle
+  // ],dtype=float)
+
+
+
+
+  // cv::Vec3b free_color(127, 63, 127);  // Road
+  
+  // std::string image_path = "/home/user/ws/src/birds_eye_output_seg102.png";  // 必要に応じてパスを変更
+  // make_images(free_color,image_path);
+
+
 }
 
-void LocalMapCreator::cloud_callback(const sensor_msgs::PointCloud2::ConstPtr &msg)
-{
-  // Transform point cloud to the frame of the local map
-  geometry_msgs::TransformStamped transform_stamped;
-  while (ros::ok())
-  {
-    try
-    {
-      transform_stamped = tf_buffer_.lookupTransform(frame_id_, msg->header.frame_id, ros::Time(0));
-      break;
-    }
-    catch (tf2::TransformException &ex)
-    {
-      ROS_WARN("%s", ex.what());
-      ros::Duration(0.5).sleep();
-    }
-  }
-  sensor_msgs::PointCloud2 cloud_transformed;
-  Eigen::Matrix4f transform = tf2::transformToEigen(transform_stamped.transform).matrix().cast<float>();
-  pcl_ros::transformPointCloud(transform, *msg, cloud_transformed);
 
-  // Create local map
-  nav_msgs::OccupancyGrid local_map = create_map(cloud_transformed, precast_db_);
+
+void LocalMapCreator::timer_callback(const ros::TimerEvent &)
+{
+  cv::Vec3b free_color(127, 63, 127);  // Road
+  
+  std::string image_path = "/home/user/ws/src/birds_eye_output_seg102.png";  // 必要に応じてパスを変更
+  // This function is intentionally left empty.
+  // It can be used for periodic tasks if needed.
+  nav_msgs::OccupancyGrid local_map = make_images(free_color,image_path, precast_db_);
   local_map.header.frame_id = frame_id_;
   local_map.header.stamp = ros::Time::now();
   map_pub_.publish(local_map);
 }
+
+
 
 PrecastDB LocalMapCreator::create_precast_db(const float map_reso, const float map_size, const float yaw_reso)
 {
@@ -107,32 +136,6 @@ int LocalMapCreator::calc_angle_id(float angle, const float yaw_reso)
   return static_cast<int>(floor(angle / yaw_reso));
 }
 
-nav_msgs::OccupancyGrid LocalMapCreator::create_map(const sensor_msgs::PointCloud2 &cloud, const PrecastDB &precast_db)
-{
-  pcl::PointCloud<pcl::PointXYZ> pcl_cloud;
-  pcl::fromROSMsg(cloud, pcl_cloud);
-  nav_msgs::OccupancyGrid local_map = init_map(precast_db.info);
-
-  for (const auto &point : pcl_cloud.points)
-  {
-    if (!in_map(point.x, point.y, precast_db.info))
-      continue;
-
-    const float dist = hypot(point.x, point.y);
-    const float angle = atan2(point.y, point.x);
-    const int angle_id = calc_angle_id(angle, precast_db.yaw_reso);
-
-    const std::vector<PrecastData> &precast_data = precast_db.bins[angle_id];
-    for (const auto &data : precast_data)
-    {
-      if (dist < data.dist)
-        local_map.data[data.grid_index] = -1;  // unknown
-    }
-    local_map.data[xy_to_grid_index(point.x, point.y, precast_db.info)] = 100;  // occupied
-  }
-
-  return local_map;
-}
 
 nav_msgs::OccupancyGrid LocalMapCreator::init_map(const nav_msgs::MapMetaData &map_info)
 {
@@ -145,17 +148,65 @@ nav_msgs::OccupancyGrid LocalMapCreator::init_map(const nav_msgs::MapMetaData &m
   return local_map;
 }
 
-bool LocalMapCreator::in_map(const float x, const float y, const nav_msgs::MapMetaData &map_info)
-{
-  return map_info.origin.position.x <= x && x <= map_info.origin.position.x + map_info.width * map_info.resolution &&
-         map_info.origin.position.y <= y && y <= map_info.origin.position.y + map_info.height * map_info.resolution;
-}
 
+//参考になるかも
 int LocalMapCreator::xy_to_grid_index(const float x, const float y, const nav_msgs::MapMetaData &map_info)
 {
   const int index_x = static_cast<int>(floor((x - map_info.origin.position.x) / map_info.resolution));
   const int index_y = static_cast<int>(floor((y - map_info.origin.position.y) / map_info.resolution));
   return index_x + (index_y * map_info.width);
+}
+
+
+
+nav_msgs::OccupancyGrid LocalMapCreator::make_images(cv::Vec3b free_color,std::string image_path, const PrecastDB &precast_db)  // 必要に応じてパスを変更
+{
+  // // 固定された画像ファイルパス
+  // std::string image_path = "/home/user/ws/src/birds_eye_output_seg102.png";  // 必要に応じてパスを変更
+  std::cout << "hello" << std::endl;
+  nav_msgs::OccupancyGrid local_map = init_map(precast_db.info);
+  // 画像の読み込み
+  cv::Mat image = cv::imread(image_path, cv::IMREAD_COLOR);
+  if (image.empty()) {
+      std::cerr << "画像を読み込めませんでした: " << image_path << std::endl;
+  }
+  // 色の変更処理（例：赤っぽいピクセル → 青に変更）
+  // for (int y = 0; y < image.rows; ++y) {
+  //     for (int x = 0; x < image.cols; ++x) {
+  //         cv::Vec3b& pixel = image.at<cv::Vec3b>(y, x);
+  //         std::cout << "Pixel at (" << x << ", " << y << "): "
+  //                   << "B: " << static_cast<int>(pixel[0]) << ", "
+  //                   << "G: " << static_cast<int>(pixel[1]) << ", "
+  //                   << "R: " << static_cast<int>(pixel[2]) << std::endl;
+  //         if (pixel != free_color) {  // free_colorと一致するピクセルを検出
+  //             pixel[0] = 0; // Blue
+  //             pixel[1] = 0;   // Green
+  //             pixel[2] = 0;   // Red
+  //         }
+  //     }
+  // }
+
+
+   // 色の変更処理（例：赤っぽいピクセル → 青に変更）
+  for (int y = 0; y < image.rows; ++y) {
+      for (int x = 0; x < image.cols; ++x) {
+          cv::Vec3b& pixel = image.at<cv::Vec3b>(y, x);
+          std::cout << "Pixel at (" << x << ", " << y << "): "
+                    << "B: " << static_cast<int>(pixel[0]) << ", "
+                    << "G: " << static_cast<int>(pixel[1]) << ", "
+                    << "R: " << static_cast<int>(pixel[2]) << std::endl;
+          if (pixel != free_color) {  // free_colorと一致するピクセルを検出
+              local_map.data[x+y*200] = 100;  // occupied
+          }
+      }
+  }
+
+  // 画像の表示
+  cv::imshow("Modified Image", image);
+  cv::waitKey(10000); // キー入力待ち
+
+
+  return local_map;
 }
 
 int main(int argc, char *argv[])
